39344e6f8b13acc694c30919a3307a5b891f9b8c
Lucas Teske <lucas@teske.net.br>
2015-10-10 22:00:52 -0300

    Added x64 debian generator.

--- debian/debianize.sh
+++ debian/debianize.sh
@@ -40,4 +40,36 @@ rm -fr ${APPDIR}/lib/
 
 dpkg-deb -b ${DIR} ./${DEB_PKG}
 
-echo ${DEB_PKG}
+echo "Generated ${DEB_PKG}"
+
+rm -fr ${DIR}
+
+mkdir -p ${DIR}
+mkdir -p ${DEBIAN}
+mkdir -p ${APPDIR}
+mkdir -p ${LIBDIR}
+
+cat <<- EOF > ${DEBIAN}/control
+Package: ${PKG_NAME}
+Priority: optional
+Maintainer: The Unknown <unknown@anonymous.com>
+Architecture: amd64
+Description: The dragon has been released for Prime!
+Version: ${VERSION}
+Depends: apache2, libapache2-mod-php5, python, python-pip
+EOF
+
+DEB_PKG="${PKG_NAME}_${VERSION}_amd64.deb"
+
+cp postinst ${DEBIAN}
+
+cp -rf ${REPO_DIR}/* ${APPDIR}
+cp -rf ${REPO_DIR}/lib/x64/* ${LIBDIR}
+rm -fr ${APPDIR}/*.pyc
+rm -fr ${APPDIR}/lib/
+
+dpkg-deb -b ${DIR} ./${DEB_PKG}
+
+echo "Generated ${DEB_PKG}"
+
+rm -fr ${DIR}/*
