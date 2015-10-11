015eaa4ed690b3fb1922e9c1fbefdfb3e726c513
Lucas Teske <lucas@teske.net.br>
2015-10-10 21:40:51 -0300

    Added Debianize Script for i386

---
+++ debian/debianize.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PKG_NAME="primedragon"
+
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
+DIR="/tmp/package/${PKG_NAME}"
+APPDIR="${DIR}/var/www/primedragon/"
+LIBDIR="${DIR}/usr/"
+DEBIAN="${DIR}/DEBIAN"
+
+rm -rf ${DIR}
+
+mkdir -p ${DIR}
+mkdir -p ${DEBIAN}
+mkdir -p ${APPDIR}
+mkdir -p ${LIBDIR}
+
+G_REV=`git rev-parse --short=8 HEAD`
+DATE=`date +"%Y%m%d%H%M%S"`
+VERSION="1-git+${DATE}.${G_REV}~$1"
+
+cat <<- EOF > ${DEBIAN}/control
+Package: ${PKG_NAME}
+Priority: optional
+Maintainer: The Unknown <unknown@anonymous.com>
+Architecture: i386
+Description: The dragon has been released for Prime!
+Version: ${VERSION}
+Depends: apache2, libapache2-mod-php5, python, python-pip
+EOF
+
+DEB_PKG="${PKG_NAME}_${VERSION}_i386.deb"
+
+cp postinst ${DEBIAN}
+
+cp -rf ${REPO_DIR}/* ${APPDIR}
+cp -rf ${REPO_DIR}/lib/x32/* ${LIBDIR}
+rm -fr ${APPDIR}/*.pyc
+rm -fr ${APPDIR}/lib/
+
+dpkg-deb -b ${DIR} ./${DEB_PKG}
+
+echo ${DEB_PKG}
